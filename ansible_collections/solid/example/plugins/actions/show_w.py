b55c28a8a8b3c1cf1692ba68ae0a73a30e45bc7e
kksat <22549266+kksat@users.noreply.github.com>
2024-06-12 16:30:44 +0200

    add show_w plugin
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ ansible_collections/solid/example/plugins/actions/show_w.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function
+from datetime import date
+
+__metaclass__ = type
+
+from ansible.utils.display import Display
+from ansible.plugins.action import ActionBase
+
+WARRANTY = f"""
+Copyright 2024 Kirill Satarin (@kksat)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+def run_action() -> dict:
+    Display().display(WARRANTY)
+    return {
+        "changed": False,
+        "failed": False,
+        "msg": "Success",
+    }
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        return run_action()
---
+++ ansible_collections/solid/example/plugins/modules/show_w.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: show_w
+author: Kirill Satarin (@kksat)
+short_description: Show warranty
+description: |
+    The hypothetical commands `show w' and `show c' should show the appropriate parts of the General Public License.
+    Of course, your program's commands might be different; for a GUI interface, you would use an “about box”.
+version_added: 1.0.0
+seealso:
+  - name: GNU General Public License 3.0
+    link: https://www.gnu.org/licenses/gpl-3.0.en.html
+options: {}
+"""
+
+EXAMPLES = r"""
+---
+- name: Show warranty
+  sap.sap_operations.show_w:
+"""
+
+RETURN = r"""
+---
+msg:
+  description: always 'Success'
+  returned: always
+  type: str
+  sample: Success
+"""
