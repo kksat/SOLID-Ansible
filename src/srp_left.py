923f97bb0cb49bf3073d5dd163924a04eb70a7c7
kksat <22549266+kksat@users.noreply.github.com>
2024-06-14 09:12:56 +0200

    add python examples for single responsibility principle
    Signed-off-by: kksat <22549266+kksat@users.noreply.github.com>

---
+++ src/srp_left.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+
+class User:
+    def __init__(self, username: str, email: str):
+        self.username = username
+        self.email = email
+
+    def save_to_database(self):
+        print(f"Saving {self.username} to the database.")
+
+    def send_email(self, subject: str, message: str):
+        print(f"Sending email to {self.email}: {subject} - {message}")
+
+
+user = User("john_doe", "john@example.com")
+user.save_to_database()
+user.send_email("Welcome", "Thanks for signing up!")
---
+++ src/srp_right.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+
+class User:
+    def __init__(self, username: str, email: str):
+        self.username = username
+        self.email = email
+
+
+class UserRepository:
+    def save_to_database(self, user: User):
+        print(f"Saving {user.username} to the database.")
+
+
+class EmailService:
+    def send_email(self, user: User, subject: str, message: str):
+        print(f"Sending email to {user.email}: {subject} - {message}")
+
+
+user = User("john_doe", "john@example.com")
+
+user_repository = UserRepository()
+email_service = EmailService()
+
+user_repository.save_to_database(user)
+email_service.send_email(user, "Welcome", "Thanks for signing up!")
